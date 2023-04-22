771ecb92076b5310aa770d911c8362dca5b6f54c
YgorMoraes <ygorluca3@gmail.com>
2023-04-22 14:59:00 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,17 +38,17 @@ window.onscroll = () => {
 
 
 // scroll reveal////////////////
-ScrollReveal({
-  reset:true,
-  distance: '80px',
-  duration: 2000,
-  delay:200
-});
+// ScrollReveal({
+//   reset:true,
+//   distance: '80px',
+//   duration: 2000,
+//   delay:200
+// });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .skills-container, .projects-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+// ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+// ScrollReveal().reveal('.home-img, .skills-container, .projects-box, .contact form', { origin: 'bottom' });
+// ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+// ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
 const typed = new Typed('.multiple-text', {
